eba78e0995675efe87c48f3ac91180b7d272ed9c
qhahd78 <umuj0708@likelion.org>
2020-12-28 21:01:21 +0900

    Model Add

--- Blog/BlogApp/models.py
+++ Blog/BlogApp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Blog(models.Model):
+    # 변수 = 처리할 것들 (ex) title 이라는 변수는 텍스트 200자까지 들어가게 할거다
+    title = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    body = models.TextField()
+    
+    # object(1) 말고 타이틀이 뜨게 해주는 함수 
+    def __str__(self):
+        return self.title
+
+# model. 웅앵Field(웅앵)
